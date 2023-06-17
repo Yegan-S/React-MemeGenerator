3233a50801aaf8f4af1be7f0b5fdac3cf856330e
sedi <salehisediqe86@gmail.com>
2023-06-17 12:40:25 -0700

    Got random meme

--- src/Meme.js
+++ src/Meme.js
@@ -3,6 +3,12 @@ import memeData from "./memeData";
 import "./App.css";
 
  export default function Meme(){
+    function getmemeImage(){
+        const memesArray = memeData.data.memes
+        const randomNumber = Math.floor(Math.random() *memesArray.length)
+        const url = memesArray[randomNumber].url
+        alert (url)
+    }
     return(
         <main>
             <form className="form">
@@ -14,7 +20,9 @@ import "./App.css";
                 className="form--input" 
                 placeholder="Bottom text"/>
 
-                <button className="form--button">
+                <button className="form--button"
+                onClick={getmemeImage}
+                >
                     Get a new meme image 
                      </button>
             </form>
