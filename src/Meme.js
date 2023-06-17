06084d6a38a659f88fac139f4c9fafbac2776678
sedi <salehisediqe86@gmail.com>
2023-06-17 12:28:39 -0700

    Created memeData component

--- src/Meme.js
+++ src/Meme.js
@@ -1,13 +1,22 @@
 import React from "react";
+import memeData from "./memeData";
 import "./App.css";
 
  export default function Meme(){
     return(
         <main>
             <form className="form">
-                <input type="text" className="form--input" placeholder="Top text"/>
-                <input type="text" className="form--input" placeholder="Bottom text"/>
-                <button className="form--button">Get a new meme image </button>
+                <input type="text" 
+                className="form--input"
+                 placeholder="Top text"/>
+
+                <input type="text" 
+                className="form--input" 
+                placeholder="Bottom text"/>
+
+                <button className="form--button">
+                    Get a new meme image 
+                     </button>
             </form>
         </main>
     );
