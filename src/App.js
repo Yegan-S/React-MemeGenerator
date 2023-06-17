5928df9e851aea2a1d381e1803db820c4be6a12b
sedi <salehisediqe86@gmail.com>
2023-06-17 10:19:13 -0700

    Created Meme component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./Header";
+import Meme from "./Meme";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
      <Header />
-     
+     <Meme />
     </div>
   );
 }
---
+++ src/Meme.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+ export default function Meme(){
+    return(
+        <main>
+            <form className="form">
+                <input type="text" className="form--input" placeholder="Top text"/>
+                <input type="text" className="form--input" placeholder="Bottom text"/>
+                <button className="form--button">Get a new meme image </button>
+            </form>
+        </main>
+    );
+ }
