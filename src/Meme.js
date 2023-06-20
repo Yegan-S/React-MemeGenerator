15206fa1837821ec32895af2ae12143ffe3ea394
sedi <salehisediqe86@gmail.com>
2023-06-20 10:27:52 -0700

    used an async function inside useEffect

--- src/Meme.js
+++ src/Meme.js
@@ -13,9 +13,14 @@ import "./App.css";
     const [allMemes, setAllMemes] = React.useState([])
 
     React.useEffect(() =>{
-        fetch("https://api.imgflip.com/get_memes")
-        .then(res => res.json ())
-        .then(data => setAllMemes(data.data.memes))
+        async function getMemes (){
+            const res = await   fetch("https://api.imgflip.com/get_memes")
+        const data = await res.json ()
+        setAllMemes(data.data.memes)
+
+        }
+        getMemes()
+
     }, [])
 
     function getmemeImage(){
